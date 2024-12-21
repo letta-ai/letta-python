6a8ac52278c9cb78e1c8dacd3a8d97fed34f6115
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-12-21 00:12:27 +0000

    Release 0.1.2

--- src/letta/core/client_wrapper.py
+++ src/letta/core/client_wrapper.py
@@ -22,7 +22,7 @@ class BaseClientWrapper:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "letta-client",
-            "X-Fern-SDK-Version": "0.1.0",
+            "X-Fern-SDK-Version": "0.1.2",
         }
         headers["Authorization"] = f"Bearer {self._get_token()}"
         return headers
