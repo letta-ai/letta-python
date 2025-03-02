4a892dc9f311a644da12fa0de47413ed4804f1d6
cthomas <caren@letta.com>
2025-03-01 20:00:08 -0800

    feat: add custom methods to support args schema (#13)

--- src/letta_client/client.py
+++ src/letta_client/client.py
@@ -1,6 +1,8 @@
 import inspect
 import typing
+from pydantic import BaseModel, Field
 from textwrap import dedent
+from abc import abstractmethod
 
 from .base_client import AsyncLettaBase, LettaBase
 from .core.request_options import RequestOptions
@@ -25,12 +27,36 @@ class AsyncLetta(AsyncLettaBase):
         self.tools = ToolsClient(client_wrapper=self._client_wrapper)
 
 
+class BaseTool(Tool):
+    name: str = Field(..., description="The name of the function.")
+    args_schema: typing.Optional[typing.Type[BaseModel]] = Field(default=None, description="The schema for validating the tool's arguments.")
+
+    @staticmethod
+    @abstractmethod
+    def run(**kwargs) -> typing.Any:
+        """
+        Execute the tool with the provided arguments.
+
+        Parameters
+        ----------
+        **kwargs
+            Keyword arguments to pass to the tool.
+
+        Returns
+        -------
+        typing.Any
+            The result of executing the tool.
+        """
+        pass
+
+
 class ToolsClient(ToolsClientBase):
 
     def create_from_function(
         self,
         *,
         func: typing.Callable,
+        args_schema: typing.Optional[typing.Type[BaseModel]] = OMIT,
         description: typing.Optional[str] = OMIT,
         tags: typing.Optional[typing.Sequence[str]] = OMIT,
         source_type: typing.Optional[str] = OMIT,
@@ -40,9 +66,72 @@ class ToolsClient(ToolsClientBase):
         return_char_limit: typing.Optional[int] = OMIT,
         request_options: typing.Optional[RequestOptions] = None,
     ) -> Tool:
+        """
+        Create a new tool from a callable
+
+        Parameters
+        ----------
+        func : typing.Callable
+            The callable to create the tool from.
+        
+        args_schema : typing.Optional[typing.Type[BaseModel]]
+            The arguments schema of the function, as a Pydantic model.
+
+        description : typing.Optional[str]
+            The description of the tool.
+
+        tags : typing.Optional[typing.Sequence[str]]
+            Metadata tags.
+
+        source_type : typing.Optional[str]
+            The source type of the function.
+
+        json_schema : typing.Optional[typing.Dict[str, typing.Optional[typing.Any]]]
+            The JSON schema of the function (auto-generated from source_code if not provided)
+
+        return_char_limit : typing.Optional[int]
+            The maximum number of characters in the response.
+
+        request_options : typing.Optional[RequestOptions]
+            Request-specific configuration.
+
+        Returns
+        -------
+        Tool
+            Successful Response
+
+        Examples
+        --------
+        from letta_client import Letta
+
+        client = Letta(
+            token="YOUR_TOKEN",
+        )
+        
+        def add_two_numbers(a: int, b: int) -> int:
+            return a + b
+        
+        client.tools.create_from_function(
+            func=add_two_numbers,
+        )
+
+        class InventoryEntryData(BaseModel):
+            data: InventoryEntry
+            quantity_change: int
+
+        def manage_inventory(data: InventoryEntry, quantity_change: int) -> bool:
+            pass
+        
+        client.tools.create_from_function(
+            func=manage_inventory,
+            args_schema=InventoryEntryData,
+        )
+        """
         source_code = dedent(inspect.getsource(func))
+        args_json_schema = args_schema.model_json_schema() if args_schema and args_schema != OMIT else None
         return self.create(
             source_code=source_code,
+            args_json_schema=args_json_schema,
             description=description,
             tags=tags,
             source_type=source_type,
@@ -51,10 +140,12 @@ class ToolsClient(ToolsClientBase):
             request_options=request_options,
         )
 
+
     def upsert_from_function(
         self,
         *,
         func: typing.Callable,
+        args_schema: typing.Optional[typing.Type[BaseModel]] = OMIT,
         description: typing.Optional[str] = OMIT,
         tags: typing.Optional[typing.Sequence[str]] = OMIT,
         source_type: typing.Optional[str] = OMIT,
@@ -64,13 +155,154 @@ class ToolsClient(ToolsClientBase):
         return_char_limit: typing.Optional[int] = OMIT,
         request_options: typing.Optional[RequestOptions] = None,
     ) -> Tool:
+        """
+        Create or update a tool from a callable
+
+        Parameters
+        ----------
+        func : typing.Callable
+            The callable to create or update the tool from.
+        
+        args_schema : typing.Optional[typing.Type[BaseModel]]
+            The arguments schema of the function, as a Pydantic model.
+
+        description : typing.Optional[str]
+            The description of the tool.
+
+        tags : typing.Optional[typing.Sequence[str]]
+            Metadata tags.
+
+        source_type : typing.Optional[str]
+            The source type of the function.
+
+        json_schema : typing.Optional[typing.Dict[str, typing.Optional[typing.Any]]]
+            The JSON schema of the function (auto-generated from source_code if not provided)
+
+        return_char_limit : typing.Optional[int]
+            The maximum number of characters in the response.
+
+        request_options : typing.Optional[RequestOptions]
+            Request-specific configuration.
+
+        Returns
+        -------
+        Tool
+            Successful Response
+
+        Examples
+        --------
+        from letta_client import Letta
+
+        client = Letta(
+            token="YOUR_TOKEN",
+        )
+
+        def add_two_numbers(a: int, b: int) -> int:
+            return a + b
+        
+        client.tools.upsert_from_function(
+            func=add_two_numbers,
+        )
+
+        class InventoryEntryData(BaseModel):
+            data: InventoryEntry
+            quantity_change: int
+
+        def manage_inventory(data: InventoryEntry, quantity_change: int) -> bool:
+            pass
+        
+        client.tools.upsert_from_function(
+            func=manage_inventory,
+            args_schema=InventoryEntryData,
+        )
+        """
         source_code = dedent(inspect.getsource(func))
+        args_json_schema = args_schema.model_json_schema() if args_schema and args_schema != OMIT else None
         return self.upsert(
             source_code=source_code,
+            args_json_schema=args_json_schema,
             description=description,
             tags=tags,
             source_type=source_type,
             json_schema=json_schema,
             return_char_limit=return_char_limit,
+            request_options=request_options,
+        )
+    
+    def add(
+        self,
+        *,
+        tool: BaseTool,
+        request_options: typing.Optional[RequestOptions] = None,
+    ) -> Tool:
+        """
+        Add a tool to Letta from a custom Tool class
+
+        Parameters
+        ----------
+        tool : BaseTool
+            The tool object to be added.
+
+        request_options : typing.Optional[RequestOptions]
+            Request-specific configuration.
+
+        Returns
+        -------
+        Tool
+            Successful Response
+
+        Examples
+        --------
+        from letta_client import Letta
+
+        client = Letta(
+            token="YOUR_TOKEN",
+        )
+
+        class InventoryItem(BaseModel):
+            sku: str  # Unique product identifier
+            name: str  # Product name
+            price: float  # Current price
+            category: str  # Product category (e.g., "Electronics", "Clothing")
+
+        class InventoryEntry(BaseModel):
+            timestamp: int  # Unix timestamp of the transaction
+            item: InventoryItem  # The product being updated
+            transaction_id: str  # Unique identifier for this inventory update
+
+        class InventoryEntryData(BaseModel):
+            data: InventoryEntry
+            quantity_change: int  # Change in quantity (positive for additions, negative for removals)
+
+        class ManageInventoryTool(BaseTool):
+            name: str = "manage_inventory"
+            args_schema: Type[BaseModel] = InventoryEntryData
+            description: str = "Update inventory catalogue with a new data entry"
+            tags: List[str] = ["inventory", "shop"]
+
+            def run(data: InventoryEntry, quantity_change: int) -> bool:
+                '''
+                Implementation of the manage_inventory tool
+                '''
+                # implementation
+
+                print(f"Updated inventory for {data.item.name} with a quantity change of {quantity_change}")
+                return True
+                
+        client.tools.add(
+            tool=ManageInventoryTool
+        )
+        """
+        run_source = dedent(inspect.getsource(tool.run))
+        source_code = run_source.replace("def run", f"def {tool.name}")
+        args_json_schema = tool.args_schema.model_json_schema() if tool.args_schema else None
+        return self.upsert(
+            source_code=source_code,
+            args_json_schema=args_json_schema or OMIT,
+            description=tool.description or OMIT,
+            tags=tool.tags or OMIT,
+            source_type=tool.source_type or OMIT,
+            json_schema=tool.json_schema or OMIT,
+            return_char_limit=tool.return_char_limit or OMIT,
             request_options=request_options,
         )
--- tests/custom/test_client.py
+++ tests/custom/test_client.py
@@ -1,6 +1,38 @@
-from letta_client import (
-    CreateBlock,
-)
+from pydantic import BaseModel
+from typing import List, Type
+
+from letta_client import CreateBlock
+from letta_client.client import BaseTool
+
+class InventoryItem(BaseModel):
+    sku: str  # Unique product identifier
+    name: str  # Product name
+    price: float  # Current price
+    category: str  # Product category (e.g., "Electronics", "Clothing")
+
+class InventoryEntry(BaseModel):
+    timestamp: int  # Unix timestamp of the transaction
+    item: InventoryItem  # The product being updated
+    transaction_id: str  # Unique identifier for this inventory update
+
+class InventoryEntryData(BaseModel):
+    data: InventoryEntry
+    quantity_change: int  # Change in quantity (positive for additions, negative for removals)
+
+class ManageInventoryTool(BaseTool):
+    name: str = "manage_inventory"
+    args_schema: Type[BaseModel] = InventoryEntryData
+    description: str = "Update inventory catalogue with a new data entry"
+    tags: List[str] = ["inventory", "shop"]
+
+    def run(data: InventoryEntry, quantity_change: int) -> bool:
+        """
+        Implementation of the manage_inventory tool
+        """
+        # implementation
+
+        print(f"Updated inventory for {data.item.name} with a quantity change of {quantity_change}")
+        return True
 
 
 def test_create_agent(client) -> None:
@@ -19,3 +51,44 @@ def test_create_agent(client) -> None:
     assert len([a for a in agents if a.id == agent.id]) == 1
 
     client.agents.delete(agent_id=agent.id)
+
+
+def test_create_agent_with_tools(client) -> None:
+    def manage_inventory_mock(data: InventoryEntry, quantity_change: int) -> bool:
+        """
+        Implementation of the manage_inventory tool
+        """
+        print(f"Updated inventory for {data.item.name} with a quantity change of {quantity_change}")
+        return True
+    
+    tool_from_func = client.tools.upsert_from_function(
+        func=manage_inventory_mock,
+        args_schema=InventoryEntryData,
+    )
+    assert tool_from_func is not None
+
+    tool_from_class = client.tools.add(
+        tool=ManageInventoryTool(),
+    )
+    assert tool_from_class is not None
+    
+    for tool in [tool_from_func, tool_from_class]:
+        tool_return = client.tools.run_tool_from_source(
+            source_code=tool.source_code,
+            args={
+                "data": InventoryEntry(
+                    timestamp=0,
+                    item=InventoryItem(
+                        name="Item 1",
+                        sku="328jf84htgwoeidfnw4",
+                        price=9.99,
+                        category="Grocery",
+                    ),
+                    transaction_id="1234",
+                ),
+                "quantity_change": 10,
+            },
+            args_json_schema=InventoryEntryData.model_json_schema(),
+        )
+        assert tool_return is not None
+        assert tool_return.tool_return == 'True'
