f89d54802ef9fa201a89b1a93e0d5bae34f82d0b
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-05-01 00:28:27 +0000

    Release 0.1.133

--- src/letta_client/types/group.py
+++ src/letta_client/types/group.py
@@ -63,6 +63,16 @@ class Group(UncheckedBaseModel):
     
     """
 
+    max_message_buffer_length: typing.Optional[int] = pydantic.Field(default=None)
+    """
+    The desired maximum length of messages in the context window of the convo agent. This is a best effort, and may be off slightly due to user/assistant interleaving.
+    """
+
+    min_message_buffer_length: typing.Optional[int] = pydantic.Field(default=None)
+    """
+    The desired minimum length of messages in the context window of the convo agent. This is a best effort, and may be off-by-one due to user/assistant interleaving.
+    """
+
     if IS_PYDANTIC_V2:
         model_config: typing.ClassVar[pydantic.ConfigDict] = pydantic.ConfigDict(extra="allow", frozen=True)  # type: ignore # Pydantic v2
     else:
