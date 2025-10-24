75f6992bd21f5389657032cdb2ced4fd1b20ec92
Jin Peng <jinjpeng@gmail.com>
2025-10-24 16:07:33 -0700

    remove tools

--- src/letta_client/resources/tools.py
+++ src/letta_client/resources/tools.py
@@ -30,7 +30,7 @@ from pydantic import BaseModel
 from textwrap import dedent
 import inspect
 
-__all__ = ["ToolsResource", "AsyncToolsResource", "BaseTool"]
+__all__ = ["ToolsResource", "AsyncToolsResource"]
 
 
 class ToolsResource(SyncAPIResource):
